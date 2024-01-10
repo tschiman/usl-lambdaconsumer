ab8f73d569bf1a9efe136af3a5f858def5fc9bec
Tim Schimandle <tschiman@gmail.com>
2024-01-09 19:17:39 -0700

    Update with region log

--- src/main/java/com/universalsentientlife/lambdaconsumer/LambdaconsumerApplication.java
+++ src/main/java/com/universalsentientlife/lambdaconsumer/LambdaconsumerApplication.java
@@ -8,6 +8,7 @@ public class LambdaconsumerApplication implements RequestHandler<String, String>
 	@Override
 	public String handleRequest(String input, Context context) {
 		context.getLogger().log("Input: " + input);
+		context.getLogger().log("Regions: " + System.getenv("AWS_REGION"));
 		// Your processing logic
 		return "Hello, " + input;
 	}
